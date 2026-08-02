cc8a83d3a3dbcdcaefceeb23ea2a88ed31ae4ffe
erickreydespaignepaul-tech <erickreydespaignepaul@gmail.com>
2026-08-02 17:50:02 -0300

    Añadir 20 especies de animales

--- js/criaturas/criaturas.js
+++ js/criaturas/criaturas.js
@@ -2,5 +2,19 @@
 export class Criaturas {
     constructor(){
         this.lista = [];
+        this.especies = [
+            'vaca','cerdo','oveja','pollo','caballo','lobo','zorro','oso','conejo','ciervo',
+            'aguila','pez','rana','tortuga','serpiente','abeja','murcielago','jaguar','tigre','dragon'
+        ];
+    }
+
+    crear(tipo,x,y){
+        this.lista.push({tipo,x,y,vida:100,animacion:0});
+    }
+
+    actualizar(dt){
+        for(const criatura of this.lista){
+            criatura.animacion += dt;
+        }
     }
 }
